9beaabe5d50d853d08d79668caa5d5efc553e693
BZrra <lucasbezerra06@gmail.com>
2019-08-06 09:25:36 -0300

    Modulo 4 Concluido

--- JavaScript/Modulo4/desafio/Exercicio2/main.js
+++ JavaScript/Modulo4/desafio/Exercicio2/main.js
@@ -2,23 +2,32 @@ let inputElement = document.querySelector('input[name=user]');
 let buttonElement = document.querySelector('button');
 
 function buscarGitHub(){
+    buildList(['Carregando...']);
     axios.get(`https://api.github.com/users/${inputElement.value}/repos`).then(function(response){
-        buildList(response);
+        let repoNames = [];
+        response.data.map((elemento) => repoNames.push(elemento.name));
+        buildList(repoNames || []);
     }).catch(function(error){
         alert('usuário não encontrado!');
     });
 
 }
 
-function buildList(retorno){
-    let repos = retorno.data || []
-    let lista = document.createElement('ul');
-    repos.map((repo) => {
+function buildList(elementos){
+    let lista = document.querySelector('ul[name=repos]');
+    if(lista){
+        lista.innerHTML = '';
+    }else{
+        lista = document.createElement('ul');
+        lista.setAttribute('name', 'repos');
+    }
+    elementos.map((elemento) => {
         let item = document.createElement('li');
-        let text = document.createTextNode(repo.name);
+        let text = document.createTextNode(elemento);
         item.appendChild(text);
         lista.appendChild(item);
     });
     document.querySelector('body').appendChild(lista);
 
 }
+
